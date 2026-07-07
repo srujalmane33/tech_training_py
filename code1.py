d80866d481f89ed16a4180c41df4cbe9150cfc4a
SRUJAL MANE <srujalmane2@gmail.com>
2026-07-07 23:40:08 +0530

    5 question added

--- code1.py
+++ code1.py
@@ -4,21 +4,16 @@
 #     total = total+i
 # print(total)
 
-
-
-# def sum_for_loop(n):
-#     total = 0
-#     for i in range(1, n + 1):
-#         total += i
-#     return total
-
-# n = 5
-# print(f"The sum of first {n} numbers is {sum_for_loop(n)}")
+#--------------------------------------------------------------------------
 
 
 # num= "32"
 # numFlaot = float(num)
 # print("the float data is ",numFlaot," and the type of data is ",type(numFlaot))
+
+
+#--------------------------------------------------------------------------
+
 
 # account_balance = int(input("Enter your current account balance: "))
 # withdrawal_amount = int(input("Enter the amount to withdraw: "))
@@ -29,15 +24,62 @@
 #     account_balance -= withdrawal_amount
 #     print(f"Withdrawal successful. New account balance: {account_balance}")
 
-username = "shreyash"
-password ="123123"
 
 
-Loginusername= str(input("Enter your username : "))
-Loginpassword= str(input("Enter your password : "))
+#--------------------------------------------------------------------------
 
 
-if Loginusername == username and Loginpassword == password:
-    print("Login successful!")
-else:
-    print("Invalid username or password. Please try again.")
+# username = "shreyash"
+# password ="123123"
+
+
+# Loginusername= str(input("Enter your username : "))
+# Loginpassword= str(input("Enter your password : "))
+
+
+# if Loginusername == username and Loginpassword == password:
+#     print("Login successful!")
+# else:
+#     print("Invalid username or password. Please try again.")
+
+
+#--------------------------------------------------------------------------
+
+# customer_salary = float(input("Enter your salary: "))
+# if customer_salary < 10000:
+#     print("You are not eligible for a loan.")
+# elif customer_salary < 20000:
+#     print("You are eligible for a standard loan.")
+# else:
+#     print("You are eligible for a premium loan.")
+
+#--------------------------------------------------------------------------
+
+# student_marks = int(input("Enter your marks: "))
+# if student_marks >= 90:
+#     print("Grade: A")
+# elif student_marks >= 80:
+#     print("Grade: B")
+# elif student_marks >= 70:
+#     print("Grade: C")
+# elif student_marks >= 60:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+
+#--------------------------------------------------------------------------
+
+# password = "secure123"
+# inpassword = input("Enter your password: ")
+# for i in range(2):
+#     if inpassword == password:
+#         print("Access granted.")
+#         break
+#     else:
+#         print("Incorrect password. Please try again.")
+#         inpassword = input("Enter your password: ")
+
+
+#--------------------------------------------------------------------------
+
